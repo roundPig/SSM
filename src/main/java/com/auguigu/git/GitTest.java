e3d5f17d7839f175457f2cf674f6ecbdfc38e088
hpf <hpf@163.com>
2022-09-10 17:36:42 +0800

    third commit

--- src/main/java/com/auguigu/git/GitTest.java
+++ src/main/java/com/auguigu/git/GitTest.java
@@ -3,7 +3,9 @@ package com.auguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+
 
     }
 }
